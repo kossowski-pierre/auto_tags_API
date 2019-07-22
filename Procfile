2861de13029b67ddecd59fcca768108dc8b61303
kossowski-pierre <46936425+kossowski-pierre@users.noreply.github.com>
2019-07-22 15:26:05 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AutoTagAPP:app
---
+++ run.py
@@ -0,0 +1,7 @@
+#! urs/bin/envflask python
+from AutoTagAPP import app
+
+print('ok')
+if __name__ == '__main__':
+	print('let see')
+	app.run(debug=True)
